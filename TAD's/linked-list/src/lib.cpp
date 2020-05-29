75c4ba711b0b666625d87f57e7bd9f7b880c0547
fferreiralf <fferreiralf.1@gmail.com>
2020-05-29 20:03:35 -0300

    Linked List-Adicionando a função de deletar por valor

--- TAD's/linked-list/src/lib.cpp
+++ TAD's/linked-list/src/lib.cpp
@@ -14,7 +14,7 @@ void push( Node** head, int data){
 	Node* new_node = new Node();/*!Instanciamento do novo Node*/
 	new_node->data = data;/*!Atribuição do novo dado ao novo Node*/
 	new_node->next = (*head);/*!Fazendo  node como o novo head*/
-	(*head) = n;/*!Estabelecendo o novo node como o novo head*/
+	(*head) = new_node;/*!Estabelecendo o novo node como o novo head*/
 }
 
 /*! Insere um dado após o elemento refenciado
@@ -24,9 +24,52 @@ void push( Node** head, int data){
 void insert(Node* node_ref, int data){	
 	Node* new_node = new Node();/*!Instanciamento do novo nodulo*/
 	new_node->data = data;/*!Atribuição do novo dado ao novo nodulo*/
-	new_node->next = node_ref->next;
+	new_node->next = node_ref->next;/*Fazendo o novo node pegar o!*/
 	node_ref->next = new_node;
 }
+/*! Insere um dado na ultima posição da lista
+ *  @param head Referência para o head da lista
+ *  @param data Valor que será inserido após o nodulo referênciado
+*/
+void append(Node** head, int data) {
+	Node* new_node = new Node();//!< Instanciando o novo Nodulo
+	Node* last = *head; //!< Instanciando o novo n
+	new_node->data = data;
+	new_node->next = NULL;
+	
+	if(*head == NULL){
+		*head = new_node;
+		return;
+	}
+
+	while(last->next != NULL )
+		last = last->next;
+
+	last->next = new_node;
+}
+
+void deleteNode( Node** head, int key){
+	Node * temp = *head, *prev;
+
+	if(temp != NULL && temp->data == key){
+		*head = temp->next;
+		free(temp);
+		return;
+	}
+
+	while(temp != NULL && temp->data != key){
+		prev = temp;
+		temp = temp->next;
+	}
+
+	if(temp == NULL) return;
+
+	prev->next = temp->next;
+
+	free(temp);
+}
+
+
 void printList(Node *node){
 	while(node != NULL){
 		std::cout << node->data << " ";
